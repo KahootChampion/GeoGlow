9aa165d07c2e5e5a7a5e925829652e3cde5ac1ef
KahootChampion <p.harismuhammadkhan@gmail.com>
2025-03-04 15:28:26 -0700

    Just import the moon icon for now rather than all of possible regular icons from font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import GlobalStyle from "./styles/GlobalStyles";
 import Header from "./Components/Header";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { faTwitter, faFontAwesome } from "@fortawesome/free-brands-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
-  library.add(fas, faTwitter, faFontAwesome, far);
-
+  library.add(faMoon);
   return (
     <>
       <GlobalStyle></GlobalStyle>
