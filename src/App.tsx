449e864ba55ab52124b0f3976d0229e123834256
KahootChampion <p.harismuhammadkhan@gmail.com>
2025-03-04 11:36:44 -0700

    Migrate over to vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import GlobalStyle from './styles/GlobalStyles'
+import Header from './Components/Header'
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle></GlobalStyle>
+      <Header></Header>;
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,12 @@
+import StyledHeader from "./StyledHeader.js";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <h1> Where in the world?</h1>
+      <h1>Dark Mode</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
