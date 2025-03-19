634448364adfb70733c673d67d72d89977ef9cf0
KahootChampion <p.harismuhammadkhan@gmail.com>
2025-03-19 12:18:56 -0600

    Add hover effect for styled toggle

--- src/Components/Home/Header/styles/StyledToggle.tsx
+++ src/Components/Home/Header/styles/StyledToggle.tsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { DARK_MODE } from "../../../../Constants";
 
 const StyledToggle = styled.div`
   display: flex;
@@ -10,6 +11,15 @@ const StyledToggle = styled.div`
 
   @media ${(props) => props.theme.media.desktop} {
     font-size: ${(props) => props.theme.fontSize.desktop.subtitle};
+  }
+
+  &:hover {
+    svg {
+      box-shadow: ${(props) =>
+        props.theme.siteTheme === DARK_MODE
+          ? `5px 5px 20px 0px ${props.theme.darkModeColors.darkModeText}`
+          : `5px 5px 20px 0px ${props.theme.lightModeColors.lightModeText}`};
+    }
   }
 `;
 
