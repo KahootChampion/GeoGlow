177a1f171e57a8c009635bf2e6a037b46495130b
KahootChampion <p.harismuhammadkhan@gmail.com>
2025-03-19 15:23:03 -0600

    minor styling adjustment for the margins on the country components

--- src/Components/Home/Body/CountryDataComponent/styles/StyledCountryInformationContainer.tsx
+++ src/Components/Home/Body/CountryDataComponent/styles/StyledCountryInformationContainer.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 const StyledCountryInformationContainer = styled.span`
   margin-left: ${(props) => props.theme.spacing.l};
+  margin-right: ${(props) => props.theme.spacing.l};
+
   margin-top: ${(props) => props.theme.spacing.s};
 
   margin-bottom: ${(props) => props.theme.spacing.l};
--- src/Components/Home/Body/CountryDataComponent/styles/StyledCountryTitle.tsx
+++ src/Components/Home/Body/CountryDataComponent/styles/StyledCountryTitle.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const StyledCountryTitle = styled.h2`
   font-weight: 800;
   margin-left: ${(props) => props.theme.spacing.l};
+  margin-right: ${(props) => props.theme.spacing.l};
+
   margin-top: ${(props) => props.theme.spacing.m};
 
   font-size: ${(props) => props.theme.fontSize.mobile.subtitle};
