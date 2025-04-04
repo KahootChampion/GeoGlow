d023c8dd1ac5048aeb7d668ae383ea5f736b3067
KahootChampion <p.harismuhammadkhan@gmail.com>
2025-04-03 19:06:14 -0600

    Hardcode triggerw width for filter component

--- app/src/Components/Shared/Filter/styles/StyledTrigger.tsx
+++ app/src/Components/Shared/Filter/styles/StyledTrigger.tsx
@@ -5,7 +5,7 @@ import { DARK_MODE } from "../../../../Constants";
 const StyledTrigger = styled(Select.Trigger)`
   font-size: ${(props) => props.theme.fontSize.mobile.subtitle};
   position: relative;
-  width: 120px;
+  width: 250px;
 
   cursor: pointer;
   display: flex;
@@ -44,7 +44,6 @@ const StyledTrigger = styled(Select.Trigger)`
   @media ${(props) => props.theme.media.desktop} {
     font-size: ${(props) => props.theme.fontSize.desktop.subtitle};
     padding-right: ${(props) => props.theme.spacing.xl};
-    width: 250px;
   }
 `;
 export default StyledTrigger;
