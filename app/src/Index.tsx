d0713b5c0c5d131333364e8056322391b30d9402
KahootChampion <p.harismuhammadkhan@gmail.com>
2025-04-03 19:39:53 -0600

    Removed unnecessary imports

--- app/src/Index.tsx
+++ app/src/Index.tsx
@@ -1,13 +1,4 @@
-import React from "react";
 import GlobalStyle from "./styles/GlobalStyles";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
-import {
-  faCheck,
-  faChevronDown,
-  faChevronUp,
-} from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
 import { SiteThemeContext } from "./SiteThemeContext";
 import { ThemeProvider } from "styled-components";
